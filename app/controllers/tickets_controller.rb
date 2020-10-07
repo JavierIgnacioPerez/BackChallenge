1b29b1bd85b2be21ee98f82c6f186a053bd437b6
Javier Pérez <javier.perez.ca@usach.cl>
2020-10-07 20:37:07 -0300

    Feat: CRUD Done

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,29 @@
 class TicketsController < ApplicationController
+
+  def index
+    @tickets = Ticket.all
+
+    render json: @tickets, status: :ok
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    @ticket.save
+    render json: @ticket, status: :created
+  end
+
+  def destroy
+    @ticket = Ticket.where(id: params[:id]).first
+    if @ticket.destroy
+      head(:Ok)
+    else
+      head(:Error)
+    end
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:event_id, :client_name, :client_mail, :confirmation_code)
+  end
+
 end
